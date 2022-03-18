5bef46ed16a4ce6306c84f21bc1ad64b4cc65cd5
Daniil <dadyarri@gmail.com>
2022-03-18 10:44:06 +0300

    style(sql): reformat code

--- structure.sql
+++ structure.sql
@@ -1,11 +1,11 @@
 create table account (
-    id uuid primary key default gen_random_uuid(),
-    email text not null
-        constraint email_pattern
-            check(email ~ '^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$')
+  id uuid primary key default gen_random_uuid(),
+  email text not null
+    constraint email_pattern
+      check(email ~ '^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$')
         unique,
-    passwordHash text not null,
-    lastActiveDate timestamp null
+  passwordHash text not null,
+  lastActiveDate timestamp null
 );
 
 create table inviteCode (
