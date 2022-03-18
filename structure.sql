e2f905a4f1bef21eda52f906a49f823e42a929f8
Daniil <dadyarri@gmail.com>
2022-03-18 10:18:30 +0300

    fix(sql): make email unique

--- structure.sql
+++ structure.sql
@@ -2,7 +2,8 @@ create table account (
     id uuid primary key,
     email text not null
         constraint email_pattern
-            check(email ~ '^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$'),
+            check(email ~ '^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$')
+        unique,
     passwordHash text not null,
     lastActiveDate timestamp null
 );
