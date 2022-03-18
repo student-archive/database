9cfce4d779fd0731f90cd8df696cd819742aeda3
Daniil <dadyarri@gmail.com>
2022-03-18 10:58:13 +0300

    feat(sql): add group table

--- structure.sql
+++ structure.sql
@@ -12,7 +12,7 @@ create table if not exists "account"
 create table if not exists "inviteCode"
 (
     id            uuid primary key   default gen_random_uuid(),
-    accountId     uuid      null references account,
+    accountId     uuid      null references "account",
     inviteCode    text      not null,
     isValid       bool      not null default true,
     activatedDate timestamp null
@@ -23,3 +23,10 @@ create table if not exists "speciality"
     id             uuid primary key default gen_random_uuid(),
     specialityName text not null
 );
+
+create table if not exists "group"
+(
+    id           uuid primary key default gen_random_uuid(),
+    specialityId uuid not null references "speciality",
+    groupName    text not null
+)
