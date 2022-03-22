a6445b464701b0eeba686672e9d382f1d258556e
Alexandr <superzhelezov@gmail.com>
2022-03-22 12:28:29 +0300

    feat(sql): add table eventPriority

--- ironalex/structure.sql
+++ ironalex/structure.sql
@@ -1,9 +1,17 @@
 create table if not exists "event"
 (
-    "id" uuid primary key default gen_random_uuid(),
-    "eventPriorityId" uuid not null references "eventPriority",
-    "groupId" uuid not null references "group",
-    "eventText" text not null,
-    "eventDescription" text null,
-    "eventDate" timestamp not null
+    "id"               uuid primary key default gen_random_uuid(),
+    "eventPriorityId"  uuid      not null references "eventPriority",
+    "groupId"          uuid      not null references "group",
+    "eventText"        text      not null,
+    "eventDescription" text      null,
+    "eventDate"        timestamp not null
 );
+
+create table if not exists "eventPriority"
+(
+    "id"                  uuid primary key default gen_random_uuid(),
+    "priorityName"        text not null,
+    "priorityDescription" text not null
+);
+
