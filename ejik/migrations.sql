a16c705bd26c2ab939e7c78dcc04d1302f24cd29
Iogsothot <supercan33cl@gmail.com>
2022-04-02 20:49:25 +0300

    fix(sql): fix tupo

--- ejik/migrations.sql
+++ ejik/migrations.sql
@@ -1,12 +1,15 @@
-alter table "attachment" add column "checksum" text ;
-alter table "group" add column "universityId" text not null references "university";
-alter table "software" add column "description" text ;
-alter table "attachment" add column "author" text null;
+alter table "attachment"
+    add column "checksum" text;
+alter table "group"
+    add column "universityId" text not null references "university";
+alter table "software"
+    add column "description" text;
+alter table "attachment"
+    add column "author" text null;
 create table if not exists "university"
 (
-    "id"        uuid primary key default gen_random_uuid(),
-    "name"      text null
-
+    "id"   uuid primary key default gen_random_uuid(),
+    "name" text null
 
 
 );
--- ejik/structure.sql
+++ ejik/structure.sql
@@ -1,29 +1,29 @@
 create table if not exists "speciality"
 (
-    "id"             uuid primary key default gen_random_uuid(),
+    "id"              uuid primary key default gen_random_uuid(),
     "speciality_name" text not null
 );
 
 create table if not exists "employee"
 (
-    "id"         uuid primary key default gen_random_uuid(),
-    "firstn_ame"  text not null,
+    "id"          uuid primary key default gen_random_uuid(),
+    "firstn_name" text not null,
     "last_name"   text not null,
-    "patronymic" text null,
-    "email"      text null
+    "patronymic"  text null,
+    "email"       text null
         constraint email_pattern
             check (email ~ '^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$')
         unique,
-    "phone"      text null
+    "phone"       text null
         constraint phone_pattern
             check ( phone ~ '^\+[1-9]\d{1,14}$' ),
-    "link"       text null
+    "link"        text null
 
 );
 
 create table if not exists "employee_position"
 (
-    "id"             uuid primary key default gen_random_uuid(),
+    "id"       uuid primary key default gen_random_uuid(),
     "position" text not null
 );
 
@@ -31,7 +31,7 @@ create table if not exists "employee_employee_position"
 (
     "employee_position_id" uuid references "employee_position" on update cascade on delete set null,
 
-    "employee_id"   uuid references "employee" on update cascade on delete set null,
+    "employee_id"          uuid references "employee" on update cascade on delete set null,
     constraint employee_position_employee_pkey primary key ("employee_position_id", "employee_id")
 );
 
@@ -44,11 +44,11 @@ create table if not exists "speciality_employee"
 
 create table if not exists "certificate"
 (
-    "id"                     uuid primary key default gen_random_uuid(),
+    "id"                      uuid primary key default gen_random_uuid(),
     "employee_id"             uuid not null references "employee",
     "certificate_name"        text not null,
     "certificate_description" text not null,
-    "office"                 text not null
+    "office"                  text not null
 );
 
 create table if not exists "employee_certificate"
@@ -60,8 +60,8 @@ create table if not exists "employee_certificate"
 
 create table if not exists "software"
 (
-    "id"        uuid primary key default gen_random_uuid(),
-    "link"      text null,
+    "id"          uuid primary key default gen_random_uuid(),
+    "link"        text null,
     "description" text not null
 
 
@@ -69,16 +69,16 @@ create table if not exists "software"
 
 create table if not exists "attachment_type"
 (
-    "id"       uuid primary key default gen_random_uuid(),
+    "id"        uuid primary key default gen_random_uuid(),
     "type_name" text not null
 );
 
 create table if not exists "attachment"
 (
-    "id"     uuid primary key default gen_random_uuid(),
-    "type_id" uuid not null references "attachment_type",
-    "link"   text null,
-    "author" text null,
+    "id"       uuid primary key default gen_random_uuid(),
+    "type_id"  uuid not null references "attachment_type",
+    "link"     text null,
+    "author"   text null,
     "checksum" text not null
 
 );
