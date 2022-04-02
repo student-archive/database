22be1ee790713659e6676a1b50339b449b76c527
Alexandr <superzhelezov@gmail.com>
2022-04-02 19:11:33 +0300

    feat(sql):add some fields

--- ironalex/structure.sql
+++ ironalex/structure.sql
@@ -1,22 +1,23 @@
 create table if not exists "event_priority"
 (
-    "id"                  uuid primary key default gen_random_uuid(),
+    "id"                   uuid primary key default gen_random_uuid(),
     "priority_name"        text not null,
     "priority_description" text not null
 );
 
 create table if not exists "event"
 (
-    "id"               uuid primary key default gen_random_uuid(),
-    "event_priority_id"  uuid      not null references "event_priority",
-    "group_id"          uuid      not null references "group",
+    "id"                uuid primary key default gen_random_uuid(),
+    "event_priority_id" uuid      not null references "event_priority",
+    "group_id"          uuid      not null references "group" check ( user_id == null ),
+    "user_id"           uuid      not null references "user" check ( group_id == null ),
     "event_text"        text      not null,
-    "eventDescription" text      null,
+    "event_description" text      null,
     "event_date"        timestamp not null
 );
 create table if not exists "trash"
 (
-    "id"          uuid primary key default gen_random_uuid(),
+    "id"           uuid primary key default gen_random_uuid(),
     "group_id"     uuid      not null references "group",
     "deleted_id"   uuid      not null,
     "deleted_date" timestamp not null
@@ -24,41 +25,45 @@ create table if not exists "trash"
 
 create table if not exists "quiz"
 (
-    "id"              uuid primary key default gen_random_uuid(),
+    "id"               uuid primary key default gen_random_uuid(),
     "subject_id"       uuid not null references "subject",
     "quiz_name"        text not null,
-    "quiz_description" text not null
+    "quiz_description" text not null,
+    "questions_amount" int  not null
 );
 
 create table if not exists "question"
 (
-    "id"           uuid primary key default gen_random_uuid(),
+    "id"            uuid primary key default gen_random_uuid(),
     "quiz_id"       uuid not null references "quiz",
-    "question_text" text not null
+    "question_text" text not null,
+    "correct_answers_amount" int not null,
+    "total_answers_amount" int not null
 );
 
 create table if not exists "quiz_variant"
 (
-    "id"              uuid primary key default gen_random_uuid(),
-    "question_id"      uuid    not null references "question",
+    "id"                uuid primary key default gen_random_uuid(),
+    "question_id"       uuid    not null references "question",
     "quiz_variant_text" text    not null,
-    "is_correct"       boolean not null
+    "is_correct"        boolean not null
 );
 
 create table if not exists "quiz_result"
 (
-    "id"     uuid primary key default gen_random_uuid(),
+    "id"      uuid primary key default gen_random_uuid(),
     "quiz_id" uuid not null references "quiz",
     "user_id" uuid not null references "user",
-    "result" int  not null
+    "result"  int  not null,
+    "quiz_submit_date"    timestamp not null
 );
 
 create table if not exists "quiz_history"
 (
-    "id"                uuid primary key default gen_random_uuid(),
-    "question_id"        uuid      not null references "question",
+    "id"                  uuid primary key default gen_random_uuid(),
+    "question_id"         uuid      not null references "question",
     "selected_variant_id" uuid      not null references "quiz_variant",
-    "user_id"            uuid      not null references "user",
-    "quiz_submit_date"    timestamp not null
+    "user_id"             uuid      not null references "user"
+
 );
 
