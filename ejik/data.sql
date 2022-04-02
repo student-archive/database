2523a51dcd361e2711a5fa88155dbfc2596dbcb8
Iogsothot <supercan33cl@gmail.com>
2022-04-02 19:34:07 +0300

    refact(sql): re-bild table

--- ejik/data.sql
+++ ejik/data.sql
@@ -4,49 +4,55 @@ values ('Методички'),
        ('Учебники'),
        ('Статьи');
 
-insert into "attachment" ("type_id", "link")
-values ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/s/v1/doc/I7ytkYPa88wOSnYH0DuNqjqbaYf4wTPItcodzv36n67sRkDyQ7I'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/s/v1/doc/uSiHAquVX5pi_Z9ZgX5Hmuwtn1KDXTRu6fxFt5s7CG3i9cktNWY'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/s/v1/doc/u1xvnpF85jfbtPP9ZuIAgBiaZg0zoZHDTVWYLDF-RUK-Z5QC2hs'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/doc452041075_588289056'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/doc452041075_590431890'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/doc452041075_580849301'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/doc452041075_578284991'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/s/v1/doc/3LPOqEUh-z-KBV1SN2oI2lF_SS89Szs5azgjklZZL4G3SrN2RHY'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/s/v1/doc/_rJ5Lur_rYDnZ4j-6Q0OWsryAKLJS5p24KtpIov1Nsd-DXGQzOI'),
-       ((select "id" from "attachment_type" order by random() limit 1),
-        'https://vk.com/s/v1/doc/yEA81_7cif89xkAu8gb_SCMFuY327YAHV3gIh__K6cyCW3nNF6Y');
+insert into "employee_position" ("position")
+values ('Директор'),
+       ('Проректор'),
+       ('Секретарь'),
+       ('Работник');
 
-insert into "software" ("subject_id", "link")
+insert into "attachment" ("type_id", "link","checksum")
+values ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/s/v1/doc/I7ytkYPa88wOSnYH0DuNqjqbaYf4wTPItcodzv36n67sRkDyQ7I','E60F4D895ADAC6961E1C95560F174394238D6A01'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/s/v1/doc/uSiHAquVX5pi_Z9ZgX5Hmuwtn1KDXTRu6fxFt5s7CG3i9cktNWY','E60F4D895ADAC6961E1C95560F174394238D6A02'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/s/v1/doc/u1xvnpF85jfbtPP9ZuIAgBiaZg0zoZHDTVWYLDF-RUK-Z5QC2hs','E60F4D895ADAC6961E1C95560F174394238D6A03'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/doc452041075_588289056','E60F4D895ADAC6961E1C95560F174394238D6A04'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/doc452041075_590431890','E60F4D895ADAC6961E1C95560F174394238D6A05'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/doc452041075_580849301','E60F4D895ADAC6961E1C95560F174394238D6A06'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/doc452041075_578284991','E60F4D895ADAC6961E1C95560F174394238D6A07'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/s/v1/doc/3LPOqEUh-z-KBV1SN2oI2lF_SS89Szs5azgjklZZL4G3SrN2RHY','E60F4D895ADAC6961E1C95560F174394238D6A08'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/s/v1/doc/_rJ5Lur_rYDnZ4j-6Q0OWsryAKLJS5p24KtpIov1Nsd-DXGQzOI','E60F4D895ADAC6961E1C95560F174394238D6A09'),
+       ((select "id" from "attachment_type" order by random() limit 1),
+        'https://vk.com/s/v1/doc/yEA81_7cif89xkAu8gb_SCMFuY327YAHV3gIh__K6cyCW3nNF6Y','E60F4D895ADAC6961E1C95560F174394238D6A10');
+
+insert into "software" ("subject_id", "link","description")
 values ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/rutracker'),
+        'https://torrentnote.ru/torrent-site/rutracker','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino'),
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino'),
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino'),
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino'),
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino'),
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino'),
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino'),
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino'),
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное'),
        ((select "id" from "subject" order by random() limit 1),
-        'https://torrentnote.ru/torrent-site/torrentino');
+        'https://torrentnote.ru/torrent-site/torrentino','нечто странное');
 
 insert into "page" ("link")
 values ('https://vk.com/im?peers=229126128_c18_c31_189321936&sel=c48'),
@@ -139,3 +145,15 @@ values ((select "id" from "speciality" order by random() limit 1),
         (select "id" from "employee" order by random() limit 1)),
        ((select "id" from "speciality" order by random() limit 1),
         (select "id" from "employee" order by random() limit 1));
+
+insert into "employee_employee_position" ("employee_position_id", "employee_id")
+values ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1)),
+       ((select "id" from "employee_position" order by random() limit 1), (select "id" from "employee" order by random() limit 1));
