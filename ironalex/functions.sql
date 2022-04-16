e19d260a648a416a7166f62e6b2f2e22bd38337b
ironalex <superzhelezov@gmail.com>
2022-04-17 00:32:27 +0300

    fix(sql): [WIP] function randomize_quiz_variants

--- ironalex/functions.sql
+++ ironalex/functions.sql
@@ -14,4 +14,23 @@ begin
 end;
 $$ language plpgsql;
 
-select randomize_quiz('ae25fdb6-c9a1-40d1-8432-d9859df31ede')
+select randomize_quiz('ae25fdb6-c9a1-40d1-8432-d9859df31ede');
+
+create or replace function randomize_quiz_variants(qid uuid)
+    returns table("q_text" text)
+as
+$$
+begin
+    return query (select quiz_variant_text
+            from quiz_variant
+            where question_id = qid
+            order by random()
+            limit (select total_answers_amount
+                   from question
+                   where id = qid
+                   limit 1));
+end;
+$$ language plpgsql;
+
+select randomize_quiz_variants('');
+
