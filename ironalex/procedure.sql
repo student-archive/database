e916bc97eb1c030f47c82760a393c343ed34b02a
ironalex <superzhelezov@gmail.com>
2022-04-16 18:02:23 +0300

    feat(sql): add procedure impeachment_captain

--- ironalex/procedure.sql
+++ ironalex/procedure.sql
@@ -1,10 +1,27 @@
-create or replace procedure "calculateResult" (quiz_history uuid)
-    language plpgsql
+create or replace procedure impeachment_captain (gid uuid)
 
 as
+
 $$
+
 begin
-    update "quiz_result"
-    set "selected_variant_id" =
+    update "user"
+    set role_id = (select "id" from "role" where "role_name" = 'Студент')
+    where id = (select "user"."id"
+                from "user"
+                         join "role" "r" on "r"."id" = "user"."role_id"
+                where "r"."role_name" = 'Староста'
+                  and "user"."group_id" = gid);
+    update "user"
+    set role_id = (select "id" from "role" where "role_name" = 'Староста')
+    where id = (select "user"."id"
+                from "user"
+                         join "role" "r" on "r"."id" = "user"."role_id"
+                where "r"."role_name" = 'Редактор'
+                  and "user"."group_id" = gid
+                order by "user"."last_name"
+                limit 1);
 end;
-$$
+$$ language plpgsql;
+
+call impeachment_captain('d0a1a0a3-203e-4631-bff7-42ff99d518e2');
