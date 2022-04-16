90539dea6e183122df73559cf9647652863c304e
ironalex <superzhelezov@gmail.com>
2022-04-17 00:13:33 +0300

    fix(sql): add function randomize_quiz

---
+++ ironalex/functions.sql
@@ -0,0 +1,17 @@
+create or replace function randomize_quiz(qid uuid)
+    returns table("q_text" text)
+as
+$$
+begin
+    return query (select question_text
+            from question
+            where quiz_id = qid
+            order by random()
+            limit (select questions_amount
+                   from quiz
+                   where id = qid
+                   limit 1));
+end;
+$$ language plpgsql;
+
+select randomize_quiz('ae25fdb6-c9a1-40d1-8432-d9859df31ede')
