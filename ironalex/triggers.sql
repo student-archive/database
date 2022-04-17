e43675d55877c88ca8830713dba9d59c2e7ca6fb
ironalex <superzhelezov@gmail.com>
2022-04-17 04:46:14 +0300

    feat(config): add trigger notify_male_certificate_for_militaryOffice()

--- ironalex/triggers.sql
+++ ironalex/triggers.sql
@@ -14,3 +14,45 @@ create trigger load_articles_propose_test_trigger
     for each row
 execute procedure load_articles_propose_test();
 
+
+
+create or replace function notify_male_certificate_for_militaryOffice() returns trigger as
+$$
+begin
+    if new.sex_id = 1 then
+        insert into "event"(event_priority_id, user_id, event_text, event_description,
+                            event_date)
+        values ((select id from event_priority where priority_name = 'high'), new.id,
+                'Ребята, Вам необходимо получить справку для военкомата',
+                'Получить данную справку можно с понедельника по среду в кабинете 213 в 1 корпусе. Режим работы 10:00-15:00',
+                current_timestamp);
+        return new;
+    end if;
+end;
+$$ language plpgsql;
+
+create trigger "notify_male_certificate_for_militaryOffice"
+    after insert
+    on "user"
+    for each row
+execute procedure notify_male_certificate_for_militaryOffice();
+create or replace function notify_male_certificate_for_militaryOffice() returns trigger as
+$$
+begin
+    if new.sex_id = 1 then
+        insert into "event"(event_priority_id, user_id, event_text, event_description,
+                            event_date)
+        values ((select id from event_priority where priority_name = 'high'), new.id,
+                'Ребята, Вам необходимо получить справку для военкомата',
+                'Получить данную справку можно с понедельника по среду в кабинете 213 в 1 корпусе. Режим работы 10:00-15:00',
+                current_timestamp);
+        return new;
+    end if;
+end;
+$$ language plpgsql;
+
+create trigger "notify_male_certificate_for_militaryOffice"
+    after insert
+    on "user"
+    for each row
+execute procedure notify_male_certificate_for_militaryOffice();
